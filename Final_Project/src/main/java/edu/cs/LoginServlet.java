bc3ebda4fbcf7d44c51f439aa3e379996083e3af
owenezz <esteveO0202@gmail.com>
2026-05-23 01:22:15 -0400

    Update LoginServlet.java

--- Final_Project/src/main/java/edu/cs/LoginServlet.java
+++ Final_Project/src/main/java/edu/cs/LoginServlet.java
@@ -1,54 +1,42 @@
 package edu.cs;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
+import java.sql.*;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 
 public class LoginServlet extends HttpServlet {
-
-    @Override
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        String sql = "SELECT id FROM users WHERE username = ? AND password = ?";
-
         try {
             AssignmentDAO dao = new AssignmentDAO();
+            Connection conn = dao.getConnection();
 
-            try (Connection conn = dao.getConnection();
-                 PreparedStatement ps = conn.prepareStatement(sql)) {
+            String sql = "SELECT id FROM users WHERE username=? AND password=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
 
-                ps.setString(1, username);
-                ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
 
-                try (ResultSet rs = ps.executeQuery()) {
-
-                    if (rs.next()) {
-                        HttpSession session = request.getSession();
-                        session.setAttribute("userId", rs.getInt("id"));
-                        session.setAttribute("username", username);
-
-                        response.sendRedirect("assignments?action=list");
-                    } else {
-                        response.sendRedirect("login.jsp?error=1");
-                    }
-                }
+            if (rs.next()) {
+                HttpSession session = request.getSession();
+                session.setAttribute("userId", rs.getInt("id"));
+                session.setAttribute("username", username);
+                response.sendRedirect("assignments?action=list");
+            } else {
+                response.sendRedirect("login.jsp?error=1");
             }
 
         } catch (Exception e) {
             e.printStackTrace();
             response.sendRedirect("login.jsp?error=1");
         }
+    }
+}
     }
 }
